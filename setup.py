212464aa9dd0c9f572cea9e4af6dbe0d39fa6713
Kavitha Chetana Didugu <34020019+kavithacd@users.noreply.github.com>
2020-10-12 12:53:53 +0530

    Update setup.py to include tree-lime name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='lime',
-      version='0.2.0.1',
-      description='Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
+setup(name='tree_lime',
+      version='1',
+      description='Tree-based Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
       url='http://github.com/marcotcr/lime',
-      author='Marco Tulio Ribeiro',
-      author_email='marcotcr@gmail.com',
+      author='Kavitha Chetana Didugu building on Marco Tulio Ribeiro',
+      author_email='kavithacd@iima.ac.in',
       license='BSD',
       packages=find_packages(exclude=['js', 'node_modules', 'tests']),
       python_requires='>=3.5',
